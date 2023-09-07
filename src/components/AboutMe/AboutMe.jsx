9ae4d0ad5ac78f5492561959596f60b5c0da315c
JuanCruz <juancruzgenovese22@gmail.com>
2023-09-07 18:58:06 -0300

    Work in progress

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -6,7 +6,8 @@ const AboutMe = () => {
     <div className={style.ConteinerImagen}>
         <div className={style.ConteinerSombra}>
             <div className={style.escrito}>
-                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur commodi omnis iusto ex perspiciatis eum similique veritatis molestias vel</p>
+                <p>I am a person who goes fast... my terapist told me that it is anxiety (lol).
+                 <br /> However, when coupled with my strong drive for personal growth, continuous learning, and resilience, it has propelled me significantly closer to my goals, while also enhancing my overall peace of mind</p>
             </div>
             <div className={style.separador}/>
             <img src="/Face2.jpg" className={style.imagen}/>
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -4,8 +4,21 @@ import style from "./Work.module.css";
 const Work = () => {
   return (
     <div className={style.ContainerImagen}>
-        <div className={style.ContainerBackground}>
+      <h1 className={style.ContainerImagenh1}> Work </h1>
+      <div className={style.elNorte}>
+        <img src="/Faso2.jpg" className={style.imagen}/>
+        <div className={style.elNorteTexto}>
+          <h1 className={style.elNorteTextoh1}> EL NORTE CULTIVA <br /> CANNABIS CLUB </h1>
+          <p> an ecommerce serviced by an AI </p>
         </div>
+      </div>
+      <div className={style.Pokemon}>
+        <img src="/Portada.jpg" className={style.pokemonImagen}/>
+        <div className={style.pokemonTexto}>
+          <h1 className={style.pokemonTextoh1}> POKEDEX </h1>
+          <p> Just a Pokedex</p>
+        </div>
+      </div>
     </div>
 
   )
