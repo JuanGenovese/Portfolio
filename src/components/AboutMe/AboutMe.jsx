d94e2f4c5fce4f81b1d2edf20eb4344f6c90e91a
JuanCruz <juancruzgenovese22@gmail.com>
2023-09-11 20:52:19 -0300

    Responsive par notebooks

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <div className={style.ConteinerSombra}>
             <div className={style.escrito}>
                 <p>I am a person who goes fast... my terapist told me that it is anxiety (lol).
-                 <br /> However, when coupled with my strong drive for personal growth, continuous learning, and resilience, it has propelled me significantly closer to my goals, while also enhancing my overall peace of mind</p>
+                <br/> However, when coupled with my strong drive for personal growth, continuous learning, and resilience, it has propelled me significantly closer to my goals, while also enhancing my overall peace of mind</p>
             </div>
             <div className={style.separador}/>
             <img src="/Face2.jpg" className={style.imagen}/>
