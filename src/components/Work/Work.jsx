5cf800f92372477f88683aed1963e1d16fc95d7f
JuanCruz <juancruzgenovese22@gmail.com>
2023-09-11 19:47:53 -0300

    .

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -8,22 +8,26 @@ const Work = () => {
       <div className={style.elNorte}>
         <img src="/Faso2.jpg" className={style.imagen}/>
         <div className={style.elNorteTexto}>
-          <h1 className={style.elNorteTextoh1}> EL NORTE CULTIVA <br /> CANNABIS CLUB </h1>
-          <p> an ecommerce serviced by an AI </p>
+          <h1 className={style.elNorteTextoh1}>
+            <span>EL NORTE CULTIVA</span>
+            <br/>
+            <span>CANNABIS CLUB </span>
+          </h1>
+          <p className={style.elNorteTextop}> an ecommerce serviced by an AI </p>
         </div>
       </div>
       <div className={style.Pokemon}>
         <img src="/Portada.jpg" className={style.pokemonImagen}/>
         <div className={style.pokemonTexto}>
           <h1 className={style.pokemonTextoh1}> POKEMONS APP </h1>
-          <p> Just a Pokedex</p>
+          <p className={style.pokemonTextop}> Just a Pokedex</p>
         </div>
       </div>
       <div className={style.jobPortal}>
         <img src="/FotoJobPortalX.jpg" className={style.jobPortalImagen}/>
         <div className={style.jobPortalTexto}>
           <h1 className={style.jobPortalTextoh1}> JOB PORTAL X</h1>
-          <p className={style.jobPortalTextop}> a job portal that connects companies with employees</p>
+          <p className={style.jobPortalTextop}> A job portal that connects companies with employees</p>
         </div>
       </div>
     </div>
