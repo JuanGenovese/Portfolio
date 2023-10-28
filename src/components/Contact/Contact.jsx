79b06daa1cce919a6e2f95b127ac1767839853b9
JuanCruz <juancruzgenovese22@gmail.com>
2023-10-28 17:23:21 -0300

    (src/components/Contact): Cambio <a/> por <Link/>

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import style from "./Contact.module.css"
+import style from "./Contact.module.css";
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
     <div className={style.contactConteiner}>
-        <a href="/Blog"><p className={style.email}>juancruzgenovese22@gmail.com</p></a>
+        <Link to="/Blog">
+          <p className={style.email}>juancruzgenovese22@gmail.com</p>
+        </Link>
     </div>
   )
 }
