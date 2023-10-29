32b55464fb64b84855696a67cd97bfa369ac1ef7
JuanCruz <juancruzgenovese22@gmail.com>
2023-10-28 21:02:02 -0300

    (src/component/aboutme2): cambios en el diseño

--- src/components/AboutMe2/AboutMe2.jsx
+++ src/components/AboutMe2/AboutMe2.jsx
@@ -3,14 +3,14 @@ import style from "./AboutMe2.module.css";
 
 const AboutMe2 = () => {
   return (
-    <div className={style.ConteinerImagen}>
+    <div className={style.Conteiner}>
         <div className={style.Background}>
           <form className={style.form}>
             <h2> Send me an email to talk!</h2>
             <input className={style.emailInput} type="email" placeholder='Email'/>
             <input className={style.subjectInput} type="text" placeholder='Subject'/>
             <textarea placeholder='Drop your message here :)'/>
-            <button> enviar </button>
+            <button> send </button>
           </form>
         </div>
     </div>
--- src/views/Blog/Blog.jsx
+++ src/views/Blog/Blog.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from "./Blog.module.css";
 import Datos from '../../components/Datos/Datos';
 import AboutMe2 from '../../components/AboutMe2/AboutMe2';
 
 const Blog = () => {
+  useEffect(() => {
+ window.scrollTo(0, 0)
+  }, [])
   return (
     <div className={style.blogConteiner}>
-        <video className={style.Fondo} autoPlay loop muted>
-            <source src="/Fondos/FondoBlog.mp4" type="video/mp4"></source>
-        </video>
+      <video className={style.Fondo} autoPlay loop muted>
+        <source src="/Fondos/FondoBlog.mp4" type="video/mp4"></source>
+      </video>
+      <div className={style.primeraVista}>
         <Datos/>
         <a href="/"><p className={style.nombre}>Juan Cruz Genovese</p></a>
         <a href="https://www.instagram.com/independent.software/" target='blank'><p className={style.weAsGrowers}>we as coders</p></a>
         <div className={style.presentacionConteiner}>
-            <h1 className={style.presentacion}>I just want to help you</h1>
+            <h1 className={style.presentacion}> Scroll down and contact me...</h1>
         </div>
-        <AboutMe2/>
-        
+      </div> 
+      <AboutMe2/>
+      <div className={style.relleno}></div>
     </div>
   )
 }
