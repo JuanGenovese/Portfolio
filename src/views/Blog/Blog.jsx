0319ef211e76d697cf822d75c522f8ef8d769ecb
Juan Genovese <juancruzgenovese22@gmail.com>
2025-06-04 12:02:58 -0300

    Ajustes de diseño en Blog

--- src/views/Blog/Blog.jsx
+++ src/views/Blog/Blog.jsx
@@ -13,11 +13,12 @@ const Blog = () => {
         <source src="/Fondos/FondoBlog.mp4" type="video/mp4"></source>
       </video>
       <div className={style.primeraVista}>
-        <Datos/>
-        <a href="/"><p className={style.nombre}>Juan Cruz Genovese</p></a>
-        <a href="https://www.instagram.com/independent.software/" target='blank'><p className={style.weAsGrowers}>we as coders</p></a>
+        <div className={style.nav}>
+          <a href="/"><p className={style.nombre}>Juan Cruz Genovese</p></a>
+          <a href="https://www.instagram.com/independent.software/" target='blank'><p className={style.weAsGrowers}>we as coders</p></a>
+        </div>
         <div className={style.presentacionConteiner}>
-            <h1 className={style.presentacion}>DO YOU WANT TO WORK TOGETHER? <br />OR JUST MAKE A FRIEND...</h1>
+          <h1 className={style.presentacion}>DO YOU WANT TO WORK TOGETHER? <br />OR JUST MAKE A FRIEND...</h1>
         </div>
       </div> 
       <Form/>
